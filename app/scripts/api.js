a8cf4b44e2ad34aef6c6f070522a56637e82a89f
tonyisid <tonyq.zeng@163.com>
2015-09-07 02:27:44 +0800

    修改api.js的接口地址用于测试服务器

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,7 +1,8 @@
+var API_URL = 'http://localhost:3000';
 var API_CONST = {
   user_api : {
-    login_url : '',
-    register_url : '',
+    login_url : API_URL + '/login',
+    register_url : API_URL + '/register',
   }
 }
 
