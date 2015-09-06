a5410ed497bcd8cd9053a8e6a4ef73d5bcfda681
tonyisid <tonyq.zeng@163.com>
2015-09-06 14:54:10 +0800

    增加登录界面的输入验证

--- app/scripts/components/loginPanel.jsx
+++ app/scripts/components/loginPanel.jsx
@@ -1,12 +1,28 @@
 var React = require('react');
 var Router = require('react-router');
 var Link  = Router.Link;
+var validator = require('validator');
+
+var getValidatedClass = function(validated){
+	var classString = 'form-group'
+	switch(validated){
+		case '0' :
+			break;
+		case '1' :
+			classString = classString + ' has-success';
+			break;
+		case '2' :
+			classString = classString + ' has-warning';
+			break;
+	}
+	return classString;
+};
 
 var LoginButton = React.createClass({
 	render : function(){
 		return (
 			<div className="form-group">
-    			<div className="col-sm-offset-2 col-sm-10">
+    			<div className="col-sm-offset-2 col-sm-8">
     				<div className="col-sm-5">
 						<button className="btn btn-primary btn-lg">登  录</button>
 					</div>
@@ -23,24 +39,56 @@ var LoginButton = React.createClass({
 
 
 var UserNameInput = React.createClass({
+	getInitialState : function(){
+		return {
+			message : '',
+			validated : '0'
+		};
+	},
+	handleBlur : function(event){
+		var phoneNumberVoild = validator.isMobilePhone(event.target.value, 'zh-CN');
+		if(phoneNumberVoild){
+			this.setState({message : '', validated : '1'});
+		}else{
+			this.setState({message : '请输入正确的手机号码', validated : '2'});
+		}
+	},
 	render : function(){
+		var classString = getValidatedClass(this.state.validated);
 		return(
-			<div className = "form-group">
-				<div className="col-sm-offset-2 col-sm-10">
-					<input type="text" className="form-control" placeholder="用户名" />
+			<div className = {classString}>
+				<div className="col-sm-offset-2 col-sm-8">
+					<input type="text" className="form-control" placeholder="手机号码" onBlur={this.handleBlur} value={this.props.userName}/>
 				</div>
+				<label className="control-label">{this.state.message}</label>
 			</div>
 		);
 	}
 });
 
 var UserPasswordInput = React.createClass({
+	getInitialState : function(){
+		return {
+			message : '',
+			validated : '0'
+		};
+	},
+	handleBlur : function(event){
+		var passwordVoild = validator.isLength(event.target.value, 6,18);
+		if(passwordVoild){
+			this.setState({message : '', validated : '1'});
+		}else{
+			this.setState({message : '密码长度为6-18', validated : '2'});
+		}
+	},
 	render : function(){
+		var classString = getValidatedClass(this.state.validated);
 		return(
-			<div className="form-group">
-				<div className="col-sm-offset-2 col-sm-10">
-					<input type="password" className="form-control" placeholder="用户密码" id="userPasswordInput" />
+			<div className={classString}>
+				<div className="col-sm-offset-2 col-sm-8">
+					<input type="password" className="form-control" placeholder="用户密码" id="userPasswordInput" onBlur={this.handleBlur}/>
 				</div>
+				<label className="control-label">{this.state.message}</label>
 			</div>
 		);
 	}
@@ -51,7 +99,7 @@ var RememberMeCheck = React.createClass({
 	render : function(){
 		return(
 			<div className="form-group">
-			    <div className="col-sm-offset-2 col-sm-10">
+			    <div className="col-sm-offset-2 col-sm-8">
 			      <div className="checkbox">
 			        <label>
 			          <input type="checkbox" /> 记住我的登录信息
