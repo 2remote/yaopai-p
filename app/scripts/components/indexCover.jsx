9cb2d40b3c34054ad9cc9b492604a71f876d7555
yangyunxin <850801584@qq.com>
2015-09-30 17:10:32 +0800

    订单管理-被预约

--- app/scripts/components/indexCover.jsx
+++ app/scripts/components/indexCover.jsx
@@ -1,14 +1,13 @@
 var React = require('react');
 
 var IndexCover = React.createClass({
-  
   render : function(){
-		return (
-				<div className="centerContent">
-				{this.props.children}
-				</div>
-			);
-	}
+    return (
+        <div className="centerContent">
+        {this.props.children}
+        </div>
+      );
+  }
 });
 
 module.exports = IndexCover;
