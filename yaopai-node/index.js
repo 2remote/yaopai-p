dcd563c5008013c6724e5203e19c72bbb0cba903
Hu Songtao <idisblueflash@gmail.com>
2015-12-02 10:59:02 +0800

    配置支持heroku动态端口

--- yaopai-node/index.js
+++ yaopai-node/index.js
@@ -45,6 +45,6 @@ app.post('/login',function(req,res){
 });
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
