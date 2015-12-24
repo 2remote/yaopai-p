8a3a7dcf41e4aed91fb597c8d86c269a332b4d17
Haitao <liubiantao@gmail.com>
2015-12-24 17:17:39 +0800

    照片遮罩：删除，封面，上移，下移样式

--- app/scripts/components/albums/index.js
+++ app/scripts/components/albums/index.js
@@ -102,47 +102,111 @@ var Albums = React.createClass({
   render: function () {
     var mainStyle = {
       prev: {
-        width: '100%',
+        width: 600,
         paddingLeft: '7px',
         position: 'absolute',
-        left: 0,
         top: 0,
-        background: 'rgba(0,0,0,.1)',
         color: '#fefff9',
+        opacity: '0',
+        background: '-webkit-gradient(linear, left bottom, left top, from(rgba(0,0,0,0)), to(rgba(0,0,0,0.6)))',
+        background: '-webkit-linear-gradient(bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.6) 100%)',
+        background: '-moz-linear-gradient(bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.6) 100%)',
+        background: 'linear-gradient(to top, rgba(0,0,0,0) 0%,rgba(0,0,0,0.6) 100%)',
+        WebkitTransition: 'opacity 0.2s',
+        MozTransition: 'opacity 0.2s',
+        transition: 'opacity 0.2s',
+        height: 50,
       },
       description: {
-        width: '100%',
+        width: 600,
         paddingLeft: '7px',
         position: 'absolute',
-        left: 0,
         bottom: 0,
-        background: 'rgba(0,0,0,.1)',
         color: '#fefff9',
         textAlign:'center',
+        opacity: '0',
+        background: '-webkit-gradient(linear, left bottom, left top, from(rgba(0,0,0,0.6)), to(rgba(0,0,0,0)))',
+        background: '-webkit-linear-gradient(bottom, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%)',
+        background: '-moz-linear-gradient(bottom, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%)',
+        background: 'linear-gradient(to top, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0) 100%)',
+        WebkitTransition: 'opacity 0.2s',
+        MozTransition: 'opacity 0.2s',
+        transition: 'opacity 0.2s',
+        height: 50,
       },
+      up:{
+        transition: 'top 0.2s',
+        fontSize: 18,
+        color: 'white',
+        float:"right",
+        textShadow: '0 0 1px rgba(0,0,0,0.6)',
+        marginRight: 20,
+        marginTop: 5,
+      },
+      down:{
+        transition: 'top 0.2s',
+        fontSize: 18,
+        color: 'white',
+        float:"right",
+        textShadow: '0 0 1px rgba(0,0,0,0.6)',
+        marginRight: 20,
+        marginTop: 15,
+      },
+      delete:{
+        transition: 'top 0.2s',
+        fontSize: 18,
+        color: 'white',
+        float:"left",
+        textShadow: '0 0 1px rgba(0,0,0,0.6)',
+        marginLeft: 20,
+        marginTop: 15,
+      },
+      spanText:{
+        padding: '0 10px 0 30px',
+        verticalAlign: 'text-bottom'
+      }
     };
     if (this.state.work && this.state.category) {
       var photoList = <div></div>
       if (this.state.work.Photos) {
         var photoList = this.state.work.Photos.map(function (photo, i) {
-          var coverStyle = {}
+          var coverStyle = {
+            transition: 'top 0.2s',
+            fontSize: 18,
+            color: 'white',
+            textShadow: '0 0 1px rgba(0,0,0,0.6)',
+            display: 'inline-block',
+            marginTop: 15,
+          }
           if (this.state.work.Cover == photo.Url) {
             coverStyle.color = "red";
           }
-          var cover = <span data-index={i} onClick={this.onCover} style={coverStyle}>设置封面</span>
+          var cover = <span style={mainStyle.spanText} data-index={i} onClick={this.onCover}>设为封面</span>
           if (coverStyle.color) {
-            cover = <span data-index={i} onClick={this.onCover} style={coverStyle}>已封面</span>
+            cover = <span style={mainStyle.spanText} data-index={i} onClick={this.onCover}>封面</span>
           }
           return (
-              <div key={i}>
-                <div style={mainStyle.prev}>
-                  <span data-index={i} onClick={this.onMoveUp} style={{float:"right"}}>上</span>
+              <div style={{width: 600, margin: 'auto'}} key={i}>
+                <div className='imghover' style={mainStyle.prev}>
+                  <span style={mainStyle.up}>
+                    <span style={{fontSize:25,marginRight:-25}} className="icon up_icon" />
+                    <span style={mainStyle.spanText}  data-index={i} onClick={this.onMoveUp} >上移</span>
+                  </span>
                 </div>
-                <img key={photo.Id} src={photo.Url}/>
-                <div style={mainStyle.description}>
-                  <span data-index={i} onClick={this.onRemove} style={{float:"left"}}>删除</span>
-                  {cover}
-                  <span data-index={i} onClick={this.onMoveDown} style={{float:"right"}}>下</span>
+                <img key={photo.Id} src={photo.Url+'?imageMogr2/auto-orient/thumbnail/600x/interlace/1'}/>
+                <div className='imghover' style={mainStyle.description}>
+                  <span style={mainStyle.delete}>
+                    <span style={{fontSize:25,marginRight:-25}} className="icon cancel_circle_icon" />
+                    <span style={mainStyle.spanText}  data-index={i} onClick={this.onRemove} >删除</span>
+                  </span>
+                  <span style={coverStyle}>
+                    <span style={{fontSize:25,marginRight:-25}} className="icon work_icon" /> 
+                    {cover}
+                  </span>
+                  <span style={mainStyle.down}>
+                    <span style={{fontSize:25,marginRight:-25}} className="icon down_icon" />
+                    <span style={mainStyle.spanText} data-index={i} onClick={this.onMoveDown} >下移</span>
+                  </span>
                 </div>
               </div>
           );
@@ -152,23 +216,24 @@ var Albums = React.createClass({
           <div className="container-fluid" style={{backgroundColor:'#111822'}}>
             <Header />
             <div style={{marginTop:100,color:'#fff'}} >
-              <div className="col-md-10">
-                <div style={{background:'url(../../../img/tag-bg.png) no-repeat',width:'100px',height:'53px',float:'left'}}>
-                  <div style={{height:'13px'}}></div>
-                  <div style={{color:'#fff',marginLeft:'15px',fontSize:'20px'}}>{this.state.category.Name}</div>
-                </div>
-                <div style={{height:'53px'}}>
-                  <div style={{height:'13px'}}></div>
-                  <div style={{color:'#fff',fontSize:'20px'}}>{this.state.currentAlbum.workData.Title}</div>
+              <div className="col-md-9">
+                <div style={{width: 600, margin: 'auto'}}>
+                  <div style={{background:'url(../../../img/tag-bg.png) no-repeat',width:'100px',height:'53px',float:'left'}}>
+                    <div style={{height:'13px'}}></div>
+                    <div style={{color:'#fff',marginLeft:'15px',fontSize:'20px'}}>{this.state.category.Name}</div>
+                  </div>
+                  <div style={{height:'53px'}}>
+                    <div style={{height:'13px'}}></div>
+                    <div style={{color:'#fff',fontSize:'20px'}}>{this.state.currentAlbum.workData.Title}</div>
+                  </div>
                 </div>
                 <ComponentGallery
                   componentStyle={{
                     width: "auto",
-                    height: "auto%",
-                    marginTop: "20px",
-                    display: "inline-block",
-                    marginRight: "10px",
-                    marginBottom: "10px",
+                    height: "auto",
+                    textAlign: 'center',
+                    display: "block",
+                    margin: "10px auto",
                     overflow: "hidden",
                     position: "relative",
                     verticalAlign: "top"
@@ -176,7 +241,7 @@ var Albums = React.createClass({
                   {photoList}
                 </ComponentGallery>
               </div>
-              <div className="col-md-2">
+              <div className="col-md-3">
                 <RightAlbumInfo work={this.state.work} uploadHandle={this.loadAlbums} categories={this.state.categories}>
                 </RightAlbumInfo>
               </div>
