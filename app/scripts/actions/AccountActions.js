bc6fa97eff9289e7a353784057e861f757bfdcdf
Hu Songtao <idisblueflash@gmail.com>
2016-01-17 22:09:16 +0800

    修复错误action修改

--- app/scripts/actions/AccountActions.js
+++ app/scripts/actions/AccountActions.js
@@ -6,7 +6,6 @@ var AccountActions = Reflux.createActions({
   'changeAvatar' : {children : ['success','failed']},
   'updateInfo' : {children : ['success','failed']},
   'userDetail' : {children : ['success','failed']},
-  'getTagList' : {children:['success','failed']},
 });
 
 AccountActions.changeAvatar.listen(function (data) {
@@ -19,13 +18,6 @@ AccountActions.updateInfo.listen(function(data){
 
 AccountActions.userDetail.listen(function(data){
   HttpFactory.post(API.USER.currentUserDetail,data,this.success,this.failed);
-});
-
-AlbumsActions.getTagList.listen(function () {
-  var data = {
-    Fields : 'id,name,display,tags.id,tags.name,tags.display'
-  }
-  HttpFactory.post(API.TAG.list,data,this.success,this.failed);
 });
 
 module.exports = AccountActions;
