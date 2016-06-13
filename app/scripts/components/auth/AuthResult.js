b8080f17501e5d809c3f030b208f0da14b2664ad
Peter Wong <amile1860@hotmail.com>
2016-06-13 17:28:59 +0800

    修改文案，增加跳转上传入口的按钮

--- app/scripts/components/auth/AuthResult.js
+++ app/scripts/components/auth/AuthResult.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 const AuthResult = React.createClass({
   render: function() {
@@ -9,9 +10,18 @@ const AuthResult = React.createClass({
             fontSize: 200,
           }}></i>
           <br />
-        <strong>认证已提交</strong>
+        <strong>认证信息已提交审核</strong>
         <br />
-        请耐心等待 1-2 上工作日审核
+        请耐心等待 1-2 个工作日。
+        <br />
+        现在您可以上传自己的作品，待认证审核通过后，将直接展示在 YAOPAI 产品库中
+        <br /><br />
+        <div style={{
+            margin: 'auto',
+            width: 220,
+          }}>
+          <Link to="/account/upload" className="btn btn-block btn-default">上传作品</Link>
+        </div>
       </div>
     );
   },
