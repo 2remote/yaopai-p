aa17ac020b7b9d7709d3a7f53ac18b07e5c4dba0
Peter Wong <amile1860@aliyun.com>
2016-06-22 16:00:14 +0800

    根据gulp官网相关recipe文档调整gulp自动化脚本

--- gulpfile.js
+++ gulpfile.js
@@ -1,170 +1,131 @@
-var gulp          = require('gulp');
+'use strict';
 
-var $             = require('gulp-load-plugins')();
-var del           = require('del');
-var source        = require('vinyl-source-stream');
-var browserify    = require('browserify');
-var preprocessify = require('preprocessify');
-var runSequence   = require('run-sequence');
-var domain        = require('domain');
-var babel = require('gulp-babel');
+var gulp = require('gulp');
+/* util */
 var gutil = require('gulp-util');
+var path = require('path');
+var del = require('del');
+var assign = require('lodash.assign');
+var exit = require('gulp-exit');
+var runSequence   = require('run-sequence');
+/* browserify */
+var watchify = require('watchify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+/* browserSync */
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-var env           = 'dev';
-var webserver     = false;
+/* **************************************************************** */
+// 参考：
+// http://gulpjs.org/recipes/fast-browserify-builds-with-watchify.html
+// https://github.com/babel/babelify#options
+// http://gulpjs.org/recipes/server-with-livereload-and-css-injection.html
+/* **************************************************************** */
 
-log = function(task, start) {
-  if (!start) {
-    setTimeout(function() {
-      $.util.log('Starting', '\'' + $.util.colors.cyan(task) + '\'...');
-    }, 1);
-  } else {
-    var time = ((new Date() - start) / 1000).toFixed(2) + ' s';
-    $.util.log('Finished', '\'' + $.util.colors.cyan(task) + '\'', 'after', $.util.colors.magenta(time));
-  }
+/* environment variables */
+var auto = process.env.YAOPAI_DISABLE_AUTO;
+var production = process.env.NODE_ENV === 'production';
+/* CONSTANTS */
+var SRC_DIR = 'app';
+var DIST_DIR = './dist';
+var ENTRY_JS = path.resolve(SRC_DIR, 'scripts', 'app.js');
+
+// add custom browserify options here
+var customOpts = {
+  entries: [ENTRY_JS],
+  // debug为true的时候babelify会报错，草，据说是sourcemap的事？
+  // https://github.com/babel/babelify#why-am-i-not-getting-source-maps
+  debug: false
 };
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
 
-gulp.task('clean:dev', function() {
-  return del(['.tmp']);
+// add transformations here
+// i.e. b.transform(coffeeify);
+b.transform(babelify, {presets: ["es2015", "react"]});
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    // Add transformation tasks to the pipeline here.
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./dist'))
+    .pipe(production ? exit() : gutil.noop());
+}
+
+gulp.task('copy-image', function() {
+  return gulp.src(path.resolve(SRC_DIR, 'img', '*'))
+    .pipe(gulp.dest(path.resolve(DIST_DIR, 'img')));
 });
 
-gulp.task('clean:dist', function() {
-  return del(['dist']);
+gulp.task('copy-vendor',function(){
+  return gulp.src(path.resolve(SRC_DIR, 'vendor', '*'))
+    .pipe(gulp.dest(path.resolve(DIST_DIR, 'vendor')));
 });
 
-gulp.task('scripts', function() {
-  var dev = env === 'dev';
-  var filePath = './app/scripts/app.js';
-  var extensions = ['.jsx'];
-
-  var bundle = function() {
-    if (dev) {
-      var start = new Date();
-      log('scripts:bundle');
-    }
-    return browserify({
-      entries: [filePath],
-      extensions: extensions,
-      debug: env === 'dev'
-    }).transform('reactify')
-    .bundle()
-      .pipe(source('app.js'))
-      .pipe(gulp.dest('.tmp/scripts/bundle'))
-      .pipe($.if(dev, $.tap(function() {
-        log('scripts:bundle', start);
-        if (!webserver) {
-          runSequence('webserver');
-        }
-      })));
-  }
-
-  if (dev) {
-    gulp.src(filePath)
-      .pipe($.plumber())
-      .pipe($.tap(function(file) {
-        var d = domain.create();
-
-        d.on('error', function(err) {
-          $.util.log($.util.colors.red('Browserify compile error:'), err.message, '\n\t', $.util.colors.cyan('in file'), file.path);
-          $.util.beep();
-        });
-
-        d.run(bundle);
-      }));
-  } else {
-    return bundle();
-  }
+gulp.task('copy-font', function() {
+  return gulp.src(path.resolve(SRC_DIR, 'fonts', '*'))
+    .pipe(gulp.dest(path.resolve(DIST_DIR, 'fonts')));
 });
 
-
-gulp.task('imagemin', function() {
-  return gulp.src(['app/img/*.png','app/img/*.jpg'])
-    .pipe(gulp.dest('dist/img'));
+gulp.task('copy-misc', function() {
+  return gulp.src([path.resolve(SRC_DIR, '*.txt'), path.resolve(SRC_DIR, '*.ico')])
+    .pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task('vendor',function(){
-  return gulp.src(['app/vendor/*.js','app/vendor/*.swf','app/vendor/*.xap'])
-    .pipe(gulp.dest('dist/vendor'));
+gulp.task('copy-css', function() {
+  return gulp.src(path.resolve(SRC_DIR, 'styles', '*'))
+    .pipe(gulp.dest(path.resolve(DIST_DIR, 'styles')));
 });
 
-gulp.task('fonts', function() {
-  return gulp.src(['app/fonts/*.eot','app/fonts/*.svg','app/fonts/*.ttf','app/fonts/*.woff','app/fonts/*.woff2'])
-    .pipe(gulp.dest('dist/fonts'));
+gulp.task('copy-html', function() {
+  return gulp.src(path.resolve(SRC_DIR, 'index.html'))
+    .pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task('copy', function() {
-  return gulp.src(['app/*.txt', 'app/*.ico'])
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('copy', ['copy-vendor', 'copy-font', 'copy-css', 'copy-image', 'copy-misc', 'copy-html']);
 
-gulp.task('copyStyles', function() {
-  return gulp.src(['app/styles/*.css', 'app/styles/*.js'])
-    .pipe(gulp.dest('dist/styles'));
-})
-
-gulp.task('bundle', function () {
-  var assets = $.useref.assets();
-  var revAll = new $.revAll({dontRenameFile: [/^\/favicon.ico$/g, '.html']});
-  var jsFilter = $.filter(['**/*.js','**/*.jsx']);
-  var cssFilter = $.filter(['**/*.css']);
-  var htmlFilter = $.filter(['*.html']);
-
-  return gulp.src('app/index.html')
-    .pipe($.preprocess())
-    .pipe(assets)
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe(jsFilter)
-    .pipe(babel({
-			presets: ['es2015']
-		}))
-    .pipe($.uglify().on('error', gutil.log))
-    .pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe($.autoprefixer({
-      browsers: ['last 5 versions']
-    }))
-    .pipe($.minifyCss())
-    .pipe(cssFilter.restore())
-    .pipe(htmlFilter)
-    .pipe($.htmlmin({collapseWhitespace: true}))
-    .pipe(htmlFilter.restore())
-    .pipe(revAll.revision())
-    .pipe(gulp.dest('dist'))
-    .pipe($.size());
-});
-
-gulp.task('webserver', function() {
-  webserver = gulp.src(['.tmp', 'app'])
-    .pipe($.webserver({
-      host: '0.0.0.0', //change to 'localhost' to disable outside connections
-      livereload: {
-        enable: true,
-        filter: function(filePath) {
-          if (/app\\(?=scripts)/.test(filePath)) {
-            $.util.log('Ignoring', $.util.colors.magenta(filePath));
-            return false;
-          } else {
-            return true;
-          }
-        }
-      },
-      open: true
-    }));
+gulp.task('clean', function() {
+  return del(DIST_DIR); // return the promise so that runSequnce won't break
 });
 
 gulp.task('serve', function() {
-  runSequence('clean:dev', ['scripts']);
-  gulp.watch('app/*.html');
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/scripts/**/*.jsx', ['scripts']);
+  browserSync({
+    port: 8000,
+    open: !auto, // env YAOPAI_DISABLE_AUTO sets this, default true
+    server: {
+      baseDir: DIST_DIR
+    },
+    ui: {
+      port: 8001
+    }
+  });
+  // could watch css as well
+  gulp.watch([path.resolve(DIST_DIR, 'bundle.js')], reload);
+});
+
+gulp.task('dev', function() {
+  production = false;
+  return runSequence('clean', ['js', 'copy'], 'serve');
 });
 
 gulp.task('build', function() {
-  env = 'prod';
-  runSequence(['clean:dev', 'clean:dist'],
-              ['scripts', ],
-              'bundle', 'copy','copyStyles','imagemin','fonts','vendor');
+  production = true;
+  return runSequence('clean', ['js', 'copy']);
 });
-
-gulp.task('default', ['build']);
