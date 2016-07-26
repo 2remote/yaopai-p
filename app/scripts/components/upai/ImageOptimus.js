3f283e295a459b8524569732cad275820d15dc37
Peter Wong <amile1860@hotmail.com>
2016-07-27 02:09:56 +0800

    调整注释和部分命名

--- app/scripts/components/upai/ImageOptimus.js
+++ app/scripts/components/upai/ImageOptimus.js
@@ -4,28 +4,20 @@ import { FILE } from '../../api'
 import UserAccountStore from '../../stores/UserAccountStore'
 
 /**
- * TODO: Ok, here is how this works:
- * √ 1. Render this component with a button/DOM element
- * √ 2. After rendering that, initialize plupload to take files from the button or sth
- * √ 3. Initialize cropperjs to use the file passed to it through plupload
- * √ 4. Now cropperjs should be able to track user's croping action
- * √ 5. When the user submits the image, it should be processed (cropped by cropperjs or so)
- *    and then passed somewhere as a binary file
- * √ 6. Now initialize QiNiu's upload utility to upload this image (using some low level uploading api, of course).
+ * // TODO: 回头我把下面的解释换成中文
  *
- * Ok, after thousands of times experimenting, dubugging, and reading, the workflow is now clear:
- *
+ * How this works:
  * √ 1. Use plupload to handle the file-choosing interface
  * √ 2. When user chooses a file through plupload, the file is handed to cropperjs for display and to crop
  * √ 3. User clicks submit, now the cropped image should be handed back to plupload
  * √ 4. Plupload uploads the cropped image to Qiniu's server
- *
  * The plupload plugin here is integrated with Qiniu's upload service, provided by their officials.
  *
+ * Note: Qiniu's plupload initialization requires user login for uptoken_url config, without it, nothing
+ * will be initialized.
  *
  * P.S. I name this component after Optimus Prime for the reason that:
- *
- * ---> There are ALWAYS times when the component needs to do MORE. It deserves such a FXXKING AWESOME name!
+ * There are ALWAYS times when the component needs to do MORE. It deserves such a FXXKING AWESOME name!
  *
 **/
 const ImageOptimus = React.createClass({
@@ -35,59 +27,54 @@ const ImageOptimus = React.createClass({
   render() {
     return (
       <div>
-        <p>
-          操作指南：<br />
-          <ol>
-            <li>点击“初始化”</li>
-            <li>点击“点我试试”</li>
-            <li>选图片文件</li>
-            <li>在剪裁区域象征性拖动几下（你不拖动几下，后面会出bug. 其实这些步骤漏哪个都可能bug，不漏也不保证没bug）</li>
-            <li>意思意思行了，点击“牛牛牛”</li>
-            <li>坐等。</li>
-          </ol>
-          结果怎么看：最后的上传结果会回显到最下面。或者用开发者工具自己看回包。
-        </p>
-        <button ref="uploader" className="btn btn-default">点我试试</button>
-        <button type="button" onClick={ this.initUploader } className="btn btn-default">初始化</button>
-        <button type="button" onClick={ this.cow } className="btn btn-default">牛牛牛</button>
+        {/* Two functional buttons */}
+        <button ref="uploader" className="btn btn-default">选择文件</button>
+        <button type="button" onClick={ this.doUpload } className="btn btn-default">开始上传</button>
+        {/* Cropperjs要求img放在一个block级别的容器里，方便加载裁剪界面 */}
         <div>
           <img ref="cropper" src={ this.state.file } style={{ maxWidth: '100%' }} />
         </div>
+        {/* 裁剪结果预览，可删除 */}
         <div>
           <img ref="croppedImage" src={ this.state.croppedImage } style={{ maxWidth: '100%' }} />
         </div>
-        <hr />
+        {/* 上传成功的返回url图片，可删除 */}
         <div>
           <img src={ this.state.uploadedImage } style={{ maxWidth: '100%' }} />
         </div>
       </div>
     )
   },
+
   cropper: undefined,
   isFileNew: false,
   uploader: undefined,
   mixins: [Reflux.connect(UserAccountStore, 'user'), Reflux.listenTo(UserAccountStore, 'onUserUpdate')],
+
   /**
    * componentDidMount和onUserUpdate负责使用sessionToken来初始化uploader
   **/
   componentDidMount() {
     this.initUploader()
   },
+
   onUserUpdate(user) {
     if(user.sessionToken) {
       this.initUploader()
     }
   },
+
   /**
    * Cropperjs只能在DOM上有img的时候才能加载，所以它的初始化逻辑是：
    * 1. Plupload加载数据之后调用FilesAdded的回调，设置state中的file
    * 2. react走生命周期的render，把file显示在<img>里
    * 3. 这里，componentDidUpdate的时候才初始化cropper
-   * 4. cropper可能是初始化也可能是换个图片重新加载，所以这里命名用了load
+   * 4. cropper可能是初始化也可能是换个图片重新加载，所以这里命名叫load
   **/
   componentDidUpdate() {
     this.loadCropper()
   },
+
   initUploader() {
     const self = this
     const { sessionToken } = self.state.user
@@ -103,7 +90,7 @@ const ImageOptimus = React.createClass({
         max_retries: 3, // 上传失败最大重试次数
         chunk_size: '4mb', // 分块上传时，每块的体积，七牛要求必须4mb
         auto_start: false, // 需要裁剪，关闭自动上传
-        multi_selection: true, // 文件选择框是否支持多选
+        multi_selection: false, // 文件选择框是否支持多选
         filters : {
           max_file_size: '10mb',
           mime_types: [
@@ -113,7 +100,7 @@ const ImageOptimus = React.createClass({
         init: {
           FilesAdded: function(up, files) {
             // 取最后一个文件
-            const rawFiles = self.uploader.files
+            const rawFiles = up.files
             self.rawId = rawFiles[rawFiles.length - 1].id
             // 读取文件
             const reader  = new FileReader()
@@ -123,7 +110,14 @@ const ImageOptimus = React.createClass({
                 file: reader.result,
               })
             }, false)
-            reader.readAsDataURL(self.uploader.getFile(self.rawId).getNative())
+            reader.readAsDataURL(up.getFile(self.rawId).getNative())
+          },
+          BeforeUpload: function(up, file) {
+            if(file.origSize >= 1024 * 1024) { // 压缩
+              up.setOption('resize', { width: 1125, height: 2208, enabled: true, quality: 90})
+            } else { // 那就不压缩了
+              up.setOption('resize', false)
+            }
           },
           UploadProgress: function(up, file) {
             // 每个文件上传时，处理相关的事情
@@ -145,7 +139,21 @@ const ImageOptimus = React.createClass({
             // 传结果给回调
             this.props.onUploadSucceed && this.props.onUploadSucceed(res.Url)
           },
-          Error: function(up, err, errTip) { },
+          Error: function(up, err, errTip) {
+            // TODO: 这段代码来源于imageInput，临时放这里，处理Error的时候参考
+            // if(this.props.onError) {
+            //   var max_file_size = plupload.parseSize('10mb');
+            //   if(err.file.size > max_file_size){
+            //     this.props.onError('您上传的图片大于 10 M，请压缩后再上传');
+            //   }else{
+            //     if(err.code == -601){
+            //       this.props.onError('图片格式错误');
+            //     }else{
+            //       this.props.onError(err.message);
+            //     }
+            //   }
+            // }
+          },
           UploadComplete: function() { },
         }
       })
@@ -153,6 +161,7 @@ const ImageOptimus = React.createClass({
       console.warn('Uploader requires a user sessionToken to initialize!')
     }
   },
+
   loadCropper() {
     const self = this
     // 新文件来了，重新加载cropper
@@ -173,7 +182,11 @@ const ImageOptimus = React.createClass({
       }
     }
   },
-  cow() {
+
+  /**
+   * 处理文件上传（到七牛）
+  **/
+  doUpload() {
     const self = this
     if(self.uploader) {
       // 清空上传列表，就是这么霸道
@@ -186,6 +199,7 @@ const ImageOptimus = React.createClass({
       })
     }
   },
+
 })
 
 export default ImageOptimus
