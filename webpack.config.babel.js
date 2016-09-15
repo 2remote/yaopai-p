8a54f5239401f87650f81a3ac343a3b0b802126f
Peter Wong <amile1860@hotmail.com>
2016-09-15 22:11:07 +0800

    调整image-webpack的优化级别为0（不优化，linux系统上优化库不存在）

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -105,7 +105,7 @@ common = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+          'image-webpack?bypassOnDebug&optimizationLevel=0&interlaced=false',
         ],
       },
       /* ================================================================ */
