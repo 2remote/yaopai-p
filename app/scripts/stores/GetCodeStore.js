7ca27065bf7e61010d37b3d887062a9a3a9c3409
slfbest@163.com <slfbest@163.com>
2016-09-21 15:35:57 +0800

    删除alert

--- app/scripts/stores/GetCodeStore.js
+++ app/scripts/stores/GetCodeStore.js
@@ -35,7 +35,6 @@ var GetCodeStore = Reflux.createStore({
     countLeft();
   },
   onBeginEmailRegister : function(data){
-    alert('1');
     console.log(data);
     this.getCode.left = 60;
     var countLeft = function(){
@@ -46,9 +45,7 @@ var GetCodeStore = Reflux.createStore({
     countLeft();
   },
   onTelRegisterSucess : function(data){
-    alert('2');
     if(data.Success){
-      alert('3');
       //this.getCode.result = '验证码已发送';
     }else{
       clearTimeout(this.timeID)
