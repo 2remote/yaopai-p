7de1c1e5e1b5757cd56df48d46b67997a2223c64
Peter Wong <amile1860@hotmail.com>
2016-09-22 23:26:47 +0800

    调整TagList的代码规范

--- app/yaopai/TagList/TagListAction.js
+++ app/yaopai/TagList/TagListAction.js
@@ -2,16 +2,21 @@ import Reflux from 'reflux'
 
 import { postStar } from 'util/HttpFactory'
 import { API_URL } from 'util/api'
+import { error } from 'util/logger'
 
 const getTagConfig = (type) => {
   const tagConfig = {
     mote: {
       url: `${API_URL}MoteAlbumsTag.List`,
-      key: `mote`,
+      key: 'mote',
     },
     ma: {
       url: `${API_URL}MakeupArtistAlbumsTag.List`,
-      key: `ma`,
+      key: 'ma',
+    },
+    pg: {
+      url: `${API_URL}Tag.List`,
+      key: 'pg',
     },
   }
 
@@ -23,24 +28,25 @@ const TagListAction = Reflux.createActions({
   get: { children: ['success', 'failure'] },
 })
 
-TagListAction.get.listen(function(type) {
-  const self = this
+TagListAction.get.listen((type) => {
+  const { success, failure } = TagListAction.get
   const tagConfig = getTagConfig(type)
   if (tagConfig) {
-    const { url, key } = tagConfig
+    const { url /* , key */ } = tagConfig
     postStar(url, {
       fields: 'Id,Name,Pinyin',
       /* 以下表示不分页 */
-      pageSize: 0, pageIndex: 0,
+      pageSize: 0,
+      pageIndex: 0,
     }, (data) => {
-      self.success(type, data.Result.map(record => ({
+      success(type, data.Result.map(record => ({
         id: record.Id,
         name: record.Name,
         phonics: record.Pinyin,
       })))
-    }, self.failure)
+    }, failure)
   } else {
-    console.error(`Unexpected tag type: [${type}]. Please check your param for [TagListAction.get].`)
+    error(`Unexpected tag type: [${type}]. Please check your param for [TagListAction.get].`)
   }
 })
 
